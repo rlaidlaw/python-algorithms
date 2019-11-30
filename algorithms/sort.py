3940dd7893b63c618e22485231a50e55d410929b
rlaidlaw <rlaidlaw@users.noreply.github.com>
2019-11-30 11:01:45 +0000

    Add selection sort

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -7,3 +7,13 @@ def bubble_sort(data):
                 data[j], data[j+1] = data[j+1], data[j]
 
     return data
+
+
+def selection_sort(data):
+    for i in range(len(data) - 1):
+        min_index = i
+        for j in range(i + 1, len(data)):
+            if data[j] < data[min_index]:
+                min_index = j
+        data[i], data[min_index] = data[min_index], data[i]
+    return data
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,10 +1,10 @@
 import unittest
-from sort import bubble_sort
+from sort import bubble_sort, selection_sort
 
 
 class SortTestCase(unittest.TestCase):
 
-    def test_bubble_sort_with_random_data(self):
+    def test_bubble_sort_with_unsorted_data(self):
         data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         actual = bubble_sort(data)
@@ -20,6 +20,24 @@ class SortTestCase(unittest.TestCase):
         data = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         actual = bubble_sort(data)
+        self.assertEqual(expected, actual)
+
+    def test_selection_sort_with_unsorted_data(self):
+        data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        actual = selection_sort(data)
+        self.assertEqual(expected, actual)
+
+    def test_selection_sort_with_sorted_data(self):
+        data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        actual = selection_sort(data)
+        self.assertEqual(expected, actual)
+
+    def test_selection_sort_with_reversed_data(self):
+        data = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        actual = selection_sort(data)
         self.assertEqual(expected, actual)
 
 
