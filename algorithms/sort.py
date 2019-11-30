772fe994eb70a062736975ea0ab638dfec587732
rlaidlaw <rlaidlaw@users.noreply.github.com>
2019-11-30 11:05:02 +0000

    Minor reformatting only

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,11 +1,9 @@
 def bubble_sort(data):
     length = len(data) - 1
-
     for i in range(length):
         for j in range(length - i):
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
-
     return data
 
 
