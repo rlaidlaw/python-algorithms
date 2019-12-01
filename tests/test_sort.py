7ecc72bddfee26e1b4c056d3c3feddaa4c868274
rlaidlaw <rlaidlaw@users.noreply.github.com>
2019-12-01 23:11:01 +0000

    Make extra tests for sort functions

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,4 +1,5 @@
 import unittest
+from random import sample
 from sort import (
     bubble_sort,
     selection_sort,
@@ -8,11 +9,47 @@ from sort import (
 )
 
 
-class SortTestCase(unittest.TestCase):
+class BubbleSortTestCase(unittest.TestCase):
 
-    def test_bubble_sort_with_unsorted_data(self):
+    def test_bubble_sort_with_unsorted_data_even(self):
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_unsorted_data_odd(self):
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        data = [2, 11, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_empty_input(self):
+        expected = []
+        data = []
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_small_input(self):
+        expected = [1]
+        data = [1]
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_large_input(self):
+        expected = list(range(1, 1001))
+        data = sample(expected, len(expected))
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_negative_data(self):
+        expected = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+        data = [-3, -5, -9, -6, -1, -10, -4, -7, -2, -8]
+        bubble_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_bubble_sort_with_mixed_sign_data(self):
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        data = [3, -5, 0, -2, -1, 1, -3, 4, 5, -4, 2]
         bubble_sort(data)
         self.assertEqual(expected, data)
 
@@ -28,9 +65,48 @@ class SortTestCase(unittest.TestCase):
         bubble_sort(data)
         self.assertEqual(expected, data)
 
-    def test_selection_sort_with_unsorted_data(self):
+
+class SelectionSortTestCase(unittest.TestCase):
+
+    def test_selection_sort_with_unsorted_data_even(self):
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_unsorted_data_odd(self):
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        data = [2, 11, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_empty_input(self):
+        expected = []
+        data = []
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_small_input(self):
+        expected = [1]
+        data = [1]
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_large_input(self):
+        expected = list(range(1, 1001))
+        data = sample(expected, len(expected))
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_negative_data(self):
+        expected = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+        data = [-3, -5, -9, -6, -1, -10, -4, -7, -2, -8]
+        selection_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_selection_sort_with_mixed_sign_data(self):
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        data = [3, -5, 0, -2, -1, 1, -3, 4, 5, -4, 2]
         selection_sort(data)
         self.assertEqual(expected, data)
 
@@ -46,9 +122,48 @@ class SortTestCase(unittest.TestCase):
         selection_sort(data)
         self.assertEqual(expected, data)
 
-    def test_merge_sort_with_unsorted_data(self):
+
+class MergeSortTestCase(unittest.TestCase):
+
+    def test_merge_sort_with_unsorted_data_even(self):
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_unsorted_data_odd(self):
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        data = [2, 11, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_empty_input(self):
+        expected = []
+        data = []
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_small_input(self):
+        expected = [1]
+        data = [1]
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_large_input(self):
+        expected = list(range(1, 1001))
+        data = sample(expected, len(expected))
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_negative_data(self):
+        expected = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+        data = [-3, -5, -9, -6, -1, -10, -4, -7, -2, -8]
+        merge_sort(data)
+        self.assertEqual(expected, data)
+
+    def test_merge_sort_with_mixed_sign_data(self):
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        data = [3, -5, 0, -2, -1, 1, -3, 4, 5, -4, 2]
         merge_sort(data)
         self.assertEqual(expected, data)
 
@@ -64,9 +179,48 @@ class SortTestCase(unittest.TestCase):
         merge_sort(data)
         self.assertEqual(expected, data)
 
-    def test_quick_sort_with_unsorted_data(self):
+
+class QuickSortTestCase(unittest.TestCase):
+
+    def test_quick_sort_with_unsorted_data_even(self):
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         data = [2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_unsorted_data_odd(self):
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        data = [2, 11, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_empty_input(self):
+        expected = []
+        data = []
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_small_input(self):
+        expected = [1]
+        data = [1]
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_large_input(self):
+        expected = list(range(1, 1001))
+        data = sample(expected, len(expected))
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_negative_data(self):
+        expected = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+        data = [-3, -5, -9, -6, -1, -10, -4, -7, -2, -8]
+        quick_sort(data, 0, len(data) - 1)
+        self.assertEqual(expected, data)
+
+    def test_quick_sort_with_mixed_sign_data(self):
+        expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+        data = [3, -5, 0, -2, -1, 1, -3, 4, 5, -4, 2]
         quick_sort(data, 0, len(data) - 1)
         self.assertEqual(expected, data)
 
@@ -82,11 +236,46 @@ class SortTestCase(unittest.TestCase):
         quick_sort(data, 0, len(data) - 1)
         self.assertEqual(expected, data)
 
-    def test_radix_sort_decimal_with_unsorted_data(self):
+
+class RadixSortTestCase(unittest.TestCase):
+
+    def test_radix_sort_decimal_with_unsorted_data_even(self):
         digits = 5
         buckets = 10
         expected = [1, 2, 33, 40, 597, 620, 701, 8909, 9678, 10123]
         data = [2, 40, 620, 8909, 10123, 9678, 701, 597, 33, 1]
+        radix_sort(data, buckets, digits)
+        self.assertEqual(expected, data)
+
+    def test_radix_sort_decimal_with_unsorted_data_odd(self):
+        digits = 5
+        buckets = 10
+        expected = [1, 2, 33, 40, 597, 620, 701, 8909, 9678, 10123, 98550]
+        data = [2, 98550, 40, 620, 8909, 10123, 9678, 701, 597, 33, 1]
+        radix_sort(data, buckets, digits)
+        self.assertEqual(expected, data)
+
+    def test_radix_sort_decimal_with_empty_input(self):
+        digits = 5
+        buckets = 10
+        expected = []
+        data = []
+        radix_sort(data, buckets, digits)
+        self.assertEqual(expected, data)
+
+    def test_radix_sort_decimal_with_small_input(self):
+        digits = 5
+        buckets = 10
+        expected = [1]
+        data = [1]
+        radix_sort(data, buckets, digits)
+        self.assertEqual(expected, data)
+
+    def test_radix_sort_decimal_with_large_input(self):
+        digits = 5
+        buckets = 10
+        expected = list(range(1, 10001))
+        data = sample(expected, len(expected))
         radix_sort(data, buckets, digits)
         self.assertEqual(expected, data)
 
